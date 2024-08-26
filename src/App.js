e00345b7c9400cc16e3284c88b55409e61b989cc
asrithasri <asrithk133@gmail.com>
2024-08-26 21:10:59 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,65 +1,73 @@
 import { useState } from "react";
-import './App.css';
+import "./App.css";
 
 function App() {
-
-  const [fullName, setFullName] = useState({});
-  const [isSubmit, setIsSubmit] = useState(false);
+  const [fullName, setFullName] = useState({
+    firstName: "",
+    lastName: "",
+  });
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
   const handleChange = (e) => {
-    let value = e.target.value;
-    setFullName({
-      ...fullName,
-      [e.target.name]: value,
-    });
+    const { name, value } = e.target;
+    setFullName((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (fullName.firstName !== undefined && fullName.lastName !== undefined) {
-      setIsSubmit(true);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { firstName, lastName } = fullName;
+
+    if (firstName.trim() && lastName.trim()) {
+      setIsSubmitted(true);
+    } else {
+      setIsSubmitted(false);
+      alert("Please fill out both first name and last name.");
     }
   };
 
   return (
-    <div>
-      <form action="submit" onSubmit={handleSubmit}>
+    <div className="app-container">
+      <form onSubmit={handleSubmit}>
         <h1>Full Name Display</h1>
-        <div>
-          <label htmlFor="firstName">
-            First Name:
-            <input
-              type='text'
-              id='firstName'
-              name='firstName'
-              onChange={handleChange}
-              placeholder="Enter first name"
-              required
-            />
-          </label>
-        </div>
-        <br />
 
-        <div>
-          <label htmlFor="lastName">
-            Last Name:
-            <input
-              type='text'
-              id='lastName'
-              name='lastName'
-              onChange={handleChange}
-              placeholder="Enter last name"
-              required
-            />
-          </label>
+        <div className="form-group">
+          <label htmlFor="firstName">First Name:</label>
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            value={fullName.firstName}
+            onChange={handleChange}
+            placeholder="Enter first name"
+            required
+          />
         </div>
-        <br />
-        <button type='submit'>Submit</button>
+
+        <div className="form-group">
+          <label htmlFor="lastName">Last Name:</label>
+          <input
+            type="text"
+            id="lastName"
+            name="lastName"
+            value={fullName.lastName}
+            onChange={handleChange}
+            placeholder="Enter last name"
+            required
+          />
+        </div>
+
+        <button type="submit">Submit</button>
       </form>
-      <br />
-      {isSubmit && (
-        <div>
-          {`Full Name: ${fullName.firstName} ${fullName.lastName}`}
+
+      {isSubmitted && (
+        <div className="result">
+          <h2>Full Name:</h2>
+          <p>
+            {fullName.firstName} {fullName.lastName}
+          </p>
         </div>
       )}
     </div>
